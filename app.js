a1c806943375e6a8acc268b955a759d805356e9a
anmol handa <handa.anmol@gmail.com>
2018-11-01 19:52:02 -0400

    made response changes

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 const express = require('express');
 const app = express();
+var path    = require("path");
 
 // Server connection
 const PORT = 9000;
 const HOST = '0.0.0.0';
 
 // Application content
-const os = ['IST 615','Cloud Management','Prof. Radhika Garg','Advance Topic Presentation']
+//const os = ['IST 615','Cloud Management','Prof. Radhika Garg','Advance Topic Presentation']
 
 // Web Server
-app.get('/Error',function(req,res) {
-  res.json(os);
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // Console output
